55d4bc5e6dc05d6a0f2b44279386d59fb76a467a
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-08-27 18:33:06 +0100

    basic counting logic for pins in the a0-a5 position

--- code/counter/counter.ino
+++ code/counter/counter.ino
@@ -1,7 +1,24 @@
-void pulsePin(int pin, int sleep=500) {
-  digitalWrite(pin, HIGH);
-  delay(sleep);
-  digitalWrite(pin, LOW);
+int LED_OFFSET = 14;
+
+bool byteOn(int time, int pos) {
+  int byteValue = (int) pow((double) 2,(double) base);
+  if(byteValue > time){
+    return false;
+  } else {
+    int divisor = byteValue * pos;
+    return (time % divisor);
+  }
+}
+
+void setTime(int time){
+  for(int i = 0; i < 6; i++) {
+    int pin = i + LED_OFFSET;
+    if(byteOn(time, pin)) {
+      digitalWrite(pin, HIGH);
+    } else {
+      digitalWrite(pin, Low);
+    }
+  }
 }
 
 void setup() {
@@ -12,8 +29,5 @@ void setup() {
 }
 
 void loop() {
-  for(int i = 0; i < 20; i++) {
-    pulsePin(i, 500);
-    delay(100);
-  }
+
 }
