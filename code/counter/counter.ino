17ab32959e49fa5cc09d9dd7143868e9c759d6fb
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-08-27 20:41:49 +0100

    let's extract everything into functions

--- code/counter/counter.ino
+++ code/counter/counter.ino
@@ -3,41 +3,26 @@ int SEG7_OFFSET = 0;
 int currentTime = 0;
 
 
-bool byteOn(int currentTime, int pinPosition) {
-  int pinValue = (int) pow(2.0, (float) pinPosition);
-  return (currentTime & pinValue);
-}
-
-bool simpleByteOn(int currentTime, int pinValue) {
-  return (currentTime & pinValue);
-}
-
 int twoTo(int exponent) {
   return 1 << exponent;
 }
 
+bool byteOn(int value, int pinPosition) {
+  int pinValue = twoTo(pinPosition);
+  return (value & pinValue);
+}
+
+void setLed(int currentTime, int pinPosition) {
+  if(byteOn(currentTime, pinPosition)) {
+    digitalWrite(LED_OFFSET + pinPosition, HIGH);
+  } else {
+    digitalWrite(LED_OFFSET + pinPosition, LOW);
+  }
+}
+
 void setTime(int currentTime){
   for(int i = 0; i < 6; i++) {
-    digitalWrite(LED_OFFSET + i, LOW);
-  }
-  delay(100);
-  if(simpleByteOn(currentTime, twoTo(0))) {
-    digitalWrite(LED_OFFSET + 0, HIGH);
-  }
-  if(simpleByteOn(currentTime, twoTo(1))) {
-    digitalWrite(LED_OFFSET + 1, HIGH);
-  }
-  if(simpleByteOn(currentTime, twoTo(2))) {
-    digitalWrite(LED_OFFSET + 2, HIGH);
-  }
-  if(simpleByteOn(currentTime, twoTo(3))) {
-    digitalWrite(LED_OFFSET + 3, HIGH);
-  }
-  if(simpleByteOn(currentTime, 16)) {
-    digitalWrite(LED_OFFSET + 4, HIGH);
-  }
-  if(simpleByteOn(currentTime, 32)) {
-    digitalWrite(LED_OFFSET + 5, HIGH);
+    setLed(currentTime, i);
   }
 }
 
@@ -49,11 +34,10 @@ void setup() {
     pinMode(pin + SEG7_OFFSET, OUTPUT);
   }
   int currentTime = 0;
-  delay(1000);
 }
 
 void loop() {
   setTime(currentTime);
   currentTime++;
-  delay(300);
+  delay(1000);
 }
