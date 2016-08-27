cd0702cfffe885e6004830d59146f3e60c244e0e
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-08-27 19:35:21 +0100

    changed to bitwise operation

--- code/counter/counter.ino
+++ code/counter/counter.ino
@@ -1,19 +1,16 @@
 int LED_OFFSET = 14;
+int currentTime = 0;
 
-bool byteOn(int time, int pos) {
-  int byteValue = (int) pow((double) 2,(double) pos);
-  if(byteValue > time){
-    return false;
-  } else {
-    int divisor = byteValue * pos;
-    return (time % divisor);
-  }
+
+bool byteOn(int currentTime, int pin) {
+  int pinValue = (int) pow(2.0, (double) pin);
+  return currentTime & pinValue;
 }
 
-void setTime(int time){
+void setTime(int currentTime){
   for(int i = 0; i < 6; i++) {
     int pin = i + LED_OFFSET;
-    if(byteOn(time, pin)) {
+    if(byteOn(currentTime, i)) {
       digitalWrite(pin, HIGH);
     } else {
       digitalWrite(pin, LOW);
@@ -25,9 +22,11 @@ void setup() {
   for(int pin = 0; pin < 20; pin++) {
     pinMode(pin, OUTPUT);
   }
-
+  int currentTime = 0;
 }
 
 void loop() {
-
+  setTime(currentTime);
+  currentTime++;
+  delay(1000);
 }
