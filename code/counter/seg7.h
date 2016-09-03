e04ba58c4429ece69af3db8838f181a2d6a9f0e5
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-09-03 16:56:47 +0100

    remove old seg7 method

--- code/counter/seg7.h
+++ code/counter/seg7.h
@@ -1,6 +1,5 @@
 namespace seg7 {
   int SEG7_OFFSET = 0;
-
   byte patterns[10] = {
     B00111111,
     B00000110,
@@ -13,17 +12,6 @@ namespace seg7 {
     B11111111,
     B11101111
   };
-
-  byte ZERO = B00111111;
-  byte ONE = B00000110;
-  byte TWO = B01011011;
-  byte THREE = B01001111;
-  byte FOUR  = B01100110;
-  byte FIVE = B01101101;
-  byte SIX = B11111101;
-  byte SEVEN = B00000111;
-  byte EIGHT = B11111111;
-  byte NINE = B11101111;
 
   void seg7Setup() {
     for(int i = 0; i < 8; i++) {
@@ -44,46 +32,6 @@ namespace seg7 {
     }
   }
 
-  void lightNumberPattern(int num) {
+  void lightNumber(int num) {
     lightByteCode(patterns[num]);
   }
-
-
-  void lightNumber(int num) {
-    byte convertedNum;
-    switch(num % 10) {
-      case 0:
-        convertedNum = ZERO;
-        break;
-      case 1:
-        convertedNum = ONE;
-        break;
-      case 2:
-        convertedNum = TWO;
-        break;
-      case 3:
-        convertedNum = THREE;
-        break;
-      case 4:
-        convertedNum = FOUR;
-        break;
-      case 5:
-        convertedNum = FIVE;
-        break;
-      case 6:
-        convertedNum = SIX;
-        break;
-      case 7:
-        convertedNum = SEVEN;
-        break;
-      case 8:
-        convertedNum = EIGHT;
-        break;
-      case 9:
-        convertedNum = NINE;
-        break;
-    }
-    lightByteCode(convertedNum);
-  }
-
-}
