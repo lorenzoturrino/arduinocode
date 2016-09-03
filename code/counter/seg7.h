8f2ffb96353005af2e0ac51afd2cc9e93aa2e39d
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-09-03 18:23:30 +0100

    extend display range to hexa

--- code/counter/seg7.h
+++ code/counter/seg7.h
@@ -1,16 +1,22 @@
 namespace seg7 {
   const int SEG7_OFFSET = 0;
-  const byte patterns[10] = {
+  const byte patterns[16] = {
     B00111111,
     B00000110,
     B01011011,
     B01001111,
     B01100110,
     B01101101,
-    B11111101,
+    B01111101,
     B00000111,
-    B11111111,
-    B11101111
+    B01111111,
+    B01101111,
+    B01110111,
+    B01111100,
+    B00111001,
+    B01011110,
+    B01111001,
+    B01110001,
   };
 
   void seg7Setup() {
@@ -33,6 +39,7 @@ namespace seg7 {
   }
 
   void lightNumber(int num) {
-    lightByteCode(patterns[num]);
+    lightByteCode(patterns[num % 16]);
   }
+
 }
