3f6bdf8912cb8b3ac8daf64fc3363a7f8cd7ef3a
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-04-08 13:10:49 +0100

    add callback

--- code/esp8266/basescript.lua
+++ code/esp8266/basescript.lua
@@ -2,6 +2,11 @@
 local WRITEPIN = 1
 gpio.mode(WRITEPIN, gpio.OUTPUT)
 
-gpio.serout(WRITEPIN, gpio.LOW, {250000, 15000}, 10)
+function serout_2()
+  gpio.mode(2, gpio.OUTPUT)
+  gpio.serout(2, gpio.LOW, {250000}, 9)
+end
+
+gpio.serout(WRITEPIN, gpio.LOW, {250000, 15000}, 10, serout_2)
 
 print('init end')
