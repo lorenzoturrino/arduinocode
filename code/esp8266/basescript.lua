9d895408e456fb4ae4383bb52f64e38a618f4849
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-04-08 13:07:50 +0100

    convert basescript to use serout

--- code/esp8266/basescript.lua
+++ code/esp8266/basescript.lua
@@ -1,14 +1,7 @@
 -- just some basic led blinking stuff
-print('init begin')
 local WRITEPIN = 1
+gpio.mode(WRITEPIN, gpio.OUTPUT)
 
-function blink()
-  print('ping')
-  gpio.write(WRITEPIN, gpio.HIGH)
-  trm.create():alarm(500, tmr.ALARM_SINGLE, function() gpio.write(WRITEPIN, gpio.LOW) end)
-end
+gpio.serout(WRITEPIN, gpio.LOW, {250000, 15000}, 10)
 
-print('init postfunction')
-gpio.mode(0, gpio.OUTPUT)
-tmr.create():alarm(3000, tmr.ALARM_AUTO, blink)
 print('init end')
